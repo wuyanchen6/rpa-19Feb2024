6d6ecc2b755860a2f90fcedcba8e873b23a80999
Teoh TeikToe <ttt@mensa.org.sg>
2024-02-20 14:22:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"]
+def main():
+  name = request.form.get("name")
+  return(render_temmplate("main.html"))
+
 if __name__ == "__main__":
   app.run()
