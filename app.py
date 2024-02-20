1601516a4a0b3e797f2f71b1b3715804a9be68f1
wuyanchen6 <160452132+wuyanchen6@users.noreply.github.com>
2024-02-20 16:11:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,23 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-r=""
-first_time = 1
-
+r = ""
+first_time =1
 @app.route("/",methods=["GET","POST"])
 def index():
   return(render_template("index.html"))
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
-global r,first_time
-if first_time==1:
-r = request.form.get("r")
-first_time=0
-return(render_template("index.html",r=r))
+  global r,first_time
+  if first_time == 1:
+    r = request.form.get("r")
+    first_time = 0
+  return(render_template("main.html",r=r))
+
 
 @app.route("/image_gpt",methods=["GET","POST"])
-def image_gpt()
-return(render_template("image_gpt.html",))
-
-
-if __name__ == "__main__":
-
-  
-  app.run()
+def image_gpt():
+  return(render_template("image_gpt.html"))
+if __name__ =="__main__":
+  app.run
