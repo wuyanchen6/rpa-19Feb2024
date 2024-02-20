93ac7be33da6687fa56fc616a6586a36db949e1f
wuyanchen6 <160452132+wuyanchen6@users.noreply.github.com>
2024-02-20 14:47:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
+@app.route("/",methods=["GET","POST"])
+def main()
+r = request.form.get("r")
+
 if __name__ == "__main__":
   app.run()
