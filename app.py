f71a331d9040563f5e241b6b37d7d0b2f8d37429
Teoh TeikToe <ttt@mensa.org.sg>
2024-02-20 15:01:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def main():
   r = request.form.get("r")
   return(render_template("main.html",r=r))
 
-@app.route("/imageGPT",methods=["GET","POST"])
+@app.route("/image_gpt",methods=["GET","POST"])
 def main():
-  return(render_template("imageGPT.html"))
+  return(render_template("image_gpt.html"))
 
 if __name__ == "__main__":
   app.run()
