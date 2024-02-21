6524d8a89a0fe36af9e0fbf72a61260b549f345c
wuyanchen6 <160452132+wuyanchen6@users.noreply.github.com>
2024-02-21 10:09:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,render_template
-
+import replicate
+import os
 app = Flask(__name__)
 
 r = ""
@@ -20,6 +21,12 @@ def main():
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
+
+
+@app.route("/image_result",methods=["GET","POST"])
+def image_result():
+    return(render_template("image_gpt.html"))
+
 
 if __name__ == "__main__":
     app.run()
