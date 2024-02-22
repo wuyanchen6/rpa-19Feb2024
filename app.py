f7f35f29da5e92cd500cf5ac7fd5f4f4a3a9ec6a
wuyanchen6 <160452132+wuyanchen6@users.noreply.github.com>
2024-02-22 09:46:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,17 @@ def image_result():
     )
     time.sleep(10)
     return(render_template("image_result.html",r=r[0]))
+@app.route("/image_result",methods=["GET","POST"])
+def image_result():
+    q = request.form.get("q")
+    r = replicate.run(
+    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    input={
+        "prompt": q,
+        }
+    )
+    time.sleep(10)
+    return(render_template("image_result.html",r=r[0]))
 
 @app.route("/end",methods=["GET","POST"])
 def end():
